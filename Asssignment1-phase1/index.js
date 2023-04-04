9f7110adecfc93797dea65a279355367843c3d0d
snehachechani01 <sneha.chechani@sigmainfo.net>
2023-04-04 15:09:33 +0530

    first commit

---
+++ Asssignment1-phase1/index.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const e = React.createElement;
+
+class LikeButton extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { liked: false };
+    }
+
+    render() {
+        if (this.state.liked) {
+            return 'You liked this.';
+        }
+
+        return e(
+            'button',
+            { onClick: () => this.setState({ liked: true }) },
+            'Click this to like '
+        );
+    }
+}
+
+const domContainer = document.querySelector('#root');
+const root = ReactDOM.createRoot(domContainer);
+root.render(e(LikeButton));
