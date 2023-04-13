4182c6f2255491789d241a57c139ebd6c7acb52f
snehachechani01 <sneha.chechani@sigmainfo.net>
2023-04-13 10:06:54 +0530

    updated

--- Phase2/assignment2/my-react-app/php/sendEmail.php
+++ Phase2/assignment2/my-react-app/php/sendEmail.php
@@ -22,7 +22,7 @@ if (isset($_POST['fname']) && isset($_POST['email']) && isset($_POST['phone']) &
         $mail->Host = 'smtp.gmail.com';
         $mail->SMTPAuth = true;
         $mail->Username = 'sneha.chechani@sigmainfo.net';
-        $mail->Password = '';  //you can enter your password here so that you will get the mail !!
+        $mail->Password = '';  
         $mail->SMTPSecure = 'tls';
         $mail->Port = 587;
 
